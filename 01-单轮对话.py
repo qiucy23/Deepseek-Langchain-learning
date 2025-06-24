5e1fa7914bd91d8119c94e70b8a48888f15e809d
chenyuQyiu <chenyu_qyiu@163.com>
2025-06-24 18:00:45 +0800

    [01]单轮对话

---
+++ 01-单轮对话.py
@@ -0,0 +1,18 @@
+from langchain_deepseek import ChatDeepSeek
+from dotenv import load_dotenv
+import os
+os.environ['http_proxy'] = 'http://127.0.0.1:7890'
+os.environ['https_proxy'] = 'http://127.0.0.1:7890'
+
+
+load_dotenv()
+
+# 初始化模型
+model = ChatDeepSeek(
+    model_name="deepseek-chat",
+    temperature=0.5,
+    max_tokens=1024,
+    api_key = "your-api-key",
+)
+
+print(model.invoke("你是谁").content)
