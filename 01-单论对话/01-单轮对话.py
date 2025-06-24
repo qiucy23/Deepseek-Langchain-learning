0bb5819d762949db5c6e4af0bf175e82b19644bf
chenyuQyiu <chenyu_qyiu@163.com>
2025-06-24 19:50:41 +0800

    01

--- 01-单论对话/01-单轮对话.py
+++ 01-单论对话/01-单轮对话.py
@@ -12,7 +12,7 @@ model = ChatDeepSeek(
     model_name="deepseek-chat",
     temperature=0.5,
     max_tokens=1024,
-    api_key = "your-api-key",
+    api_key = "my-api-key",
 )
 
 print(model.invoke("你是谁").content)
